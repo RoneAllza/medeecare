f7d60c7b53a64f17884596c1641075ac6ffedf96
Vahka Rafa Aisya <116329182+navilledhiver@users.noreply.github.com>
2024-05-20 18:25:10 +0700

    aisya update forum

---
+++ resources/views/ForumDiskusiKesehatan/homepage-sport.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <title>Komunitas Olahraga</title>
+    <style>
+    h1 {text-align: center;}
+    h2 {text-align: center;}
+    h3 {text-align: left;}
+    </style>
+  </head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" style= "color: rgb(163, 38, 56); font-size:large;"#">medeecare</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    
+    <h2 class="py-3" style="color: rgb(0, 0, 0);"> <b>Selamat datang di Komunitas Olahraga!</b></h2>
+    <p class="py-1" style="margin-top:1rem;text-align:center"> Silahkan jelajahi isi forum sesuai dengan keinginan Anda :)</p>
+
+
+    
+</body>
+</html>
