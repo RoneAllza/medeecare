982d43fcbbe5ab70b903984d31c16b0dd13a97b5
Vahka Rafa Aisya <116329182+navilledhiver@users.noreply.github.com>
2024-05-28 15:07:22 +0700

    MED-4 <update homepage forum>

--- resources/views/ForumDiskusiKesehatan/homepage-sport.php
+++ resources/views/ForumDiskusiKesehatan/homepage-sport.php
@@ -11,6 +11,10 @@
     h1 {text-align: center;}
     h2 {text-align: center;}
     h3 {text-align: left;}
+    
+    .card-text span {
+      margin-right: 10px;
+    }
     </style>
   </head>
 
@@ -29,10 +33,34 @@
         </div>
       </nav>
     
-    <h2 class="py-3" style="color: rgb(0, 0, 0);"> <b>Selamat datang di Komunitas Olahraga!</b></h2>
-    <p class="py-1" style="margin-top:1rem;text-align:center"> Silahkan jelajahi isi forum sesuai dengan keinginan Anda :)</p>
-
-
+<p class="py-1">
+<div class="container mt-5">
+  <div class="card">
+  <div class="card-header">
+  <h4 class="card-title">Komunitas Olahraga</h4>
+  </div>
+  <div class="card-body">
+  <img src="https://static.vecteezy.com/system/resources/previews/000/693/406/original/people-exercise-and-fitness-cartoon-set-vector.jpg" class="card-img-top" style="max-width: 55%;display: block; margin: 0 auto;">
+  <p class="py-1">
+  <p class="card-text text-center">
+      <span><b>9</b> Topik</span>
+      <span><b>12k</b> Interaksi</span>
+      <span><b>42k</b> Anggota</span>
+  </p>
+  <hr>
+  <p class="card-text text-center">Olahraga itu penting. Tapi bagaimana cara berolahraga yang benar? Bagikan ceritamu dan silakan bertanya pada para pakar kami tentang olahraga dan kesehatan tubuh. </p>
+  </div>
+</div>
+</div>
     
+<div class="container mt-3">
+<div class="form-floating mb-3">
+  <input type="email" class="form-control" id="floatingInputDisabled" placeholder="name@example.com" disabled>
+  <label for="floatingInputDisabled">Punya pertanyaan terkait olahraga? Silahkan klik dibawah ini untuk memposting pertanyaanmu!</label>
+</div>
+  <p class="py-1">
+  <a href="/pengisian-form" class="btn btn-dark">Post sesuatu >></a>
+</div>
+
 </body>
 </html>
